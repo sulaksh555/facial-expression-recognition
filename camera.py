9f8af64942d3cad2492e1b007936f8fbee7243ee
sulaksh555 <51309522+sulaksh555@users.noreply.github.com>
2021-12-16 04:18:38 +0530

    Update camera.py

--- camera.py
+++ camera.py
@@ -8,7 +8,7 @@ font = cv2.FONT_HERSHEY_SIMPLEX
 
 class VideoCamera(object):
     def __init__(self):
-        self.video = cv2.VideoCapture("/home/rhyme/Desktop/Project/videos/facial-exp.mkv")
+        self.video = cv2.VideoCapture("/Users/sulaksh555/Documents/github-cloned-repos/facial-expression-recognition/videos/presidential-debate.mp4")
 
     def __del__(self):
         self.video.release()
@@ -20,13 +20,13 @@ class VideoCamera(object):
         faces = facec.detectMultiScale(gray_fr, 1.3, 5)
 
         for (x, y, w, h) in faces:
-            fc = gray_fr[y:y+h, x:x+w]
+            fc = gray_fr[y : y + h, x : x + w]
 
             roi = cv2.resize(fc, (48, 48))
             pred = model.predict_emotion(roi[np.newaxis, :, :, np.newaxis])
 
             cv2.putText(fr, pred, (x, y), font, 1, (255, 255, 0), 2)
-            cv2.rectangle(fr, (x,y), (x+w,y+h), (255,0,0), 2)
+            cv2.rectangle(fr, (x, y), (x + w, y + h), (255, 0, 0), 2)
 
         _, jpeg = cv2.imencode(".jpg", fr)
         return jpeg.tobytes()
