096a051afb484918aa20be3ef4d0d5af7ca34935
sulaksh555 <51309522+sulaksh555@users.noreply.github.com>
2021-12-16 02:35:31 +0530

    Update model.py

--- model.py
+++ model.py
@@ -12,19 +12,18 @@ set_session(session)
 
 class FacialExpressionModel(object):
 
-    EMOTIONS_LIST = ["Angry", "Disgust", "Fear", "Happy",
-                     "Neutral", "Sad", "Surprise"]
+    EMOTIONS_LIST = ["Angry", "Disgust", "Fear", "Happy", "Neutral", "Sad", "Surprise"]
 
     def __init__(self, model_json_file, model_weights_file):
-        # load model from JSON file
+        # load the model from the JSON file
         with open(model_json_file, "r") as json_file:
             loaded_model_json = json_file.read()
             self.loaded_model = model_from_json(loaded_model_json)
 
-        # load weights into the new model
+        # load the weights into the new model
         self.loaded_model.load_weights(model_weights_file)
-        #self.loaded_model.compile()
-        #self.loaded_model._make_predict_function()
+        self.loaded_model.compile()
+        self.loaded_model._make_predict_function()
 
     def predict_emotion(self, img):
         global session
