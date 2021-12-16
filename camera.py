f2d11e9a2a56cb110955489427c99cb5867715df
sulaksh555 <51309522+sulaksh555@users.noreply.github.com>
2021-12-16 07:28:52 +0530

    Update camera.py

--- camera.py
+++ camera.py
@@ -8,7 +8,7 @@ font = cv2.FONT_HERSHEY_SIMPLEX
 
 class VideoCamera(object):
     def __init__(self):
-        self.video = cv2.VideoCapture("/videos/presidential-debate.mp4")
+        self.video = cv2.VideoCapture("./videos/presidential-debate.mp4")
 
     def __del__(self):
         self.video.release()
